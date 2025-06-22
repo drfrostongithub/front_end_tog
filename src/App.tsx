0ca1ac94925617fde61941183ed8d10aa9f05e16
drfrostongithub <eternal.putra@gmail.com>
2025-06-23 01:51:53 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import TagInput from "./components/TagInput";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Tag Input</h1>
+        <TagInput />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TagInput.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const TagInput = () => {
+  const [tags, setTags] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const addTag = () => {
+    const trimmedValue = inputValue.trim();
+    if (trimmedValue && !tags.includes(trimmedValue)) {
+      setTags([...tags, trimmedValue]);
+      setInputValue("");
+    }
+  };
+
+  const removeTag = (index: number) => {
+    const newTags = tags.filter((_, i) => i !== index);
+    setTags(newTags);
+  };
+
+  return (
+    <div className="tag-input">
+      <div>
+        {tags.map((tag, index) => (
+          <div key={index} className="tag">
+            <span>{tag}</span>
+            <button onClick={() => removeTag(index)}>x</button>
+          </div>
+        ))}
+      </div>
+      <input
+        type="text"
+        placeholder="Add a tag"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button type="button" onClick={addTag}>
+        Add
+      </button>
+    </div>
+  );
+};
+
+export default TagInput;
